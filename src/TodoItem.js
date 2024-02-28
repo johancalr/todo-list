b66a78c7984e0fd9e3f5034c7e7673a9b66e87e9
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-02-28 16:41:28 -0500

    agrega React-icons al proyecto

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,20 @@
 import { Button, Col, Form, ListGroup, Row } from "react-bootstrap";
+import { TiDeleteOutline } from "react-icons/ti";
 
 function TodoItem(props) {
   return (
-    <ListGroup.Item action variant="light">
+    <ListGroup.Item action variant="success" className="cursor-auto">
       <Row>
-        <Col className="col-auto">
-          <Form.Check></Form.Check>
+        <Col className="col-auto fs-4 lh-1">
+          <Form.Check role="button"></Form.Check>
         </Col>
         <Col>
-          <small>{props.text}</small>
+          <span>{props.text}</span>
         </Col>
         <Col className="col-auto">
-          <Button size="sm" className="py-0" variant="danger">X</Button>
+          <Button size="sm" className="py-0 px-1 lh-1 bg-pink border-0" variant="danger">
+            <TiDeleteOutline className="fs-4" />
+          </Button>
         </Col>
       </Row>
     </ListGroup.Item>
